4124afcf030c0ff4276cd67541d1eecdc99b24f7
coderrakibul <coder.rakibul@gmail.com>
2022-03-16 19:35:53 +0600

    site is ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const number = 5555;
 const singer = { name: "Mahfuz", job: "singer" };
@@ -42,6 +42,7 @@ function App() {
        products.map(product => <Products name={product.name} price={product.price}></Products>)
      }
      <Counter></Counter>
+     <ExternalUsers></ExternalUsers>
     </div>
     
   );
@@ -95,6 +96,33 @@ function Counter(){
       <button onClick={increaseCount}>Increase</button>
       <button onClick={decreaseCount}>Decrease</button>
     </div>
+  )
+}
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email} > </User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+   <div style={{border:"2px solid lightPink", margin:"10px", padding:"10px", borderRadius: "10px"}}>
+     <h3>Name: {props.name}</h3>
+     <p>Email: {props.email}</p>
+   </div>
   )
 }
 
